a570bab542c2986967277c1d32cc91442b75edc8
bigjunglex <gearl00taz@gmail.com>
2023-07-13 14:52:57 +0300

    notepad + sql

--- new_post.rb
+++ new_post.rb
@@ -6,19 +6,19 @@ require_relative 'memo.rb'
 puts "Hello, i'm your notepad1"
 puts "Whats your desired entry?"
 
-choices = Post.post_types
+choices = Post.post_types.keys
 
 choice = -1
 
 until choice >= 0 && choice < choices.size
-  choices.each_index do |type, index|
+  choices.each_with_index do |type, index|
     puts "\t#{index}. #{type}"
   end
 
   choice  = STDIN.gets.chomp.to_i
 end
 
-entry = Post.create(choice)
+entry = Post.create(choices[choice])
 entry.read_from_console
-entry.save
-puts "Entry saved"
+id = entry.save_to_db
+puts "Entry saved, id = #{id}"
--- task.rb
+++ task.rb
@@ -23,4 +23,18 @@ class Task < Post
     return [deadline, @text, time_string]
   end
 
+  def to_db_hash
+    return super.merge(
+        {
+            'text' => @text,
+            'due_date' => @due_date.to_s
+        }
+    )
+  end
+
+  def load_data(data_hash)
+    super(data_hash)
+    @text = data_hash['text']
+    @due_date = Date.parse(data_hash['due_date'])
+  end
 end
